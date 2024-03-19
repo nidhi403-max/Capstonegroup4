57cfa84fa01c268191ab79057d3419eab3297ad5
nidhi403-max <nidhumehta210@gmail.com>
2024-03-19 02:20:58 -0400

    commit

--- backend/Routes/Auth.js
+++ backend/Routes/Auth.js
@@ -1,66 +1,60 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-const { body, validationResult } = require("express-validator");
+const { body, validationResult } = require('express-validator');
+const axios = require('axios')
+const jwtSecret = "HaHa"
 
-router.post(
-  "/createuser",
-  [
-    body("email").isEmail(),
-    body("name").isLength({ min: 2 }),
-    body("password").isLength({ min: 2 }).withMessage("Incorrect Password"),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      await User.create({
-        name: req.body.name,
-        password: req.body.password,
-        email: req.body.email,
-        location: req.body.location,
-      });
-      res.json({ success: true });
-    } catch (error) {
-      console.error(error);
-      res.json({ success: false });
-    }
+router.post("/createuser", async (req, res) => {
+  
+  try {
+    await User.create({
+      name: req.body.name,
+      password: req.body.password,
+      email: req.body.email,
+      location: req.body.location,
+    });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false });
   }
-);
+});
 
-router.post(
-  "/loginuser",
-  [
-    body("email").isEmail(),
-    body("password").isLength({ min: 2 }).withMessage("Incorrect Password"),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    let email = req.body.email;
 
-    try {
-      let userData = await User.findOne(email);
-      if (!userData) {
-        return (
-          res.status(400), json({ errors: "Enter Correct Login credentials" })
-        );
+router.post('/login', [
+  body('email', "Enter a Valid Email").isEmail(),
+  body('password', "Password cannot be blank").exists(),
+], async (req, res) => {
+  let success = false
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+  }
+
+  const { email, password } = req.body;
+  try {
+      let user = await User.findOne({ email });  //{email:email} === {email}
+      if (!user) {
+          return res.status(400).json({ success, error: "Try Logging in with correct credentials" });
       }
-      if (req.body.password !== userData.password) {
-        return (
-          res.status(400), json({ errors: "Enter Correct Login credentials" })
-        );
+
+      const data = {
+          user: {
+              id: user.id
+          }
       }
+      success = true;
+      console.log(data);
       return res.json({ success: true });
-    } catch (error) {
-      console.error(error);
-      res.json({ success: false });
-    }
+
+      
+
+  } catch (error) {
+      console.error(error.message)
+      res.send("Server Error")
   }
-);
+})
+
 
 module.exports = router;
