a61174854d0103bbc597d0a9a6863822fde32b07
nidhi403-max <nidhumehta210@gmail.com>
2024-04-03 00:09:48 -0400

    changes for old code

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -3,46 +3,45 @@ const User = require("../models/User");
 const Event = require("../models/Events");
 const Venue = require("../models/Venue");
 
-
 const getAllEvents = async (req, res) => {
-    try {
-      const bookings = await Booking.find().lean(); 
-  
-      if (bookings.length === 0) {
-        return res.status(404).send({ message: "No bookings found" });
-      }
-  
-      const bookingData = await Promise.all(bookings.map(async (booking) => {
-        const user = await User.findById(booking.userId).lean(); 
-        const event = await Event.findById(booking.eventId).lean(); 
-        const venue = await Venue.findById(booking.venueId).lean(); 
+  try {
+    const bookings = await Booking.find().lean();
 
+    if (bookings.length === 0) {
+      return res.status(404).send({ message: "No bookings found" });
+    }
 
-  
+    const bookingData = await Promise.all(
+      bookings.map(async (booking) => {
+        const user = await User.findById(booking.userId).lean();
+        const event = await Event.findById(booking.eventId).lean();
+        const venue = await Venue.findById(booking.venueId).lean();
+
         return {
           eventDate: booking.eventDate,
           decorationPackage: booking.decorationPackage,
           specialRequests: booking.specialRequests,
           totalPrice: booking.totalPrice,
-          userEmail: user?.email, 
+          userEmail: user?.email,
           userFirstName: user?.firstname,
           userLastName: user?.lastname,
-          eventName:event?.title,
-          venueName:venue?.name,
-          venueCapacity:venue?.capacity,
-          vanueLocation:venue?.location,
-          accommodations: booking.accommodations
+          eventName: event?.title,
+          venueName: venue?.name,
+          venueCapacity: venue?.capacity,
+          vanueLocation: venue?.location,
+          accommodations: booking.accommodations,
         };
-      }));
-  
-      console.log(JSON.stringify(bookingData, null, 2));
-  
-      res.status(200).json(bookingData); 
-    } catch (error) {
-      console.error("Error fetching bookings:", error);
-      res.status(500).send({ message: "Internal server error" });
-    }
-  };
+      })
+    );
+
+    console.log(JSON.stringify(bookingData, null, 2));
+
+    res.status(200).json(bookingData);
+  } catch (error) {
+    console.error("Error fetching bookings:", error);
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
 
 const createBooking = async (req, res) => {
   try {
@@ -51,12 +50,12 @@ const createBooking = async (req, res) => {
     const booking = new Booking({
       userId: req.body.userId,
       eventId: req.body.eventId,
-      venueId:req.body.venueId,
+      venueId: req.body.venueId,
       eventDate: req.body.eventDate,
       decorationPackage: req.body.decorationPackage,
       specialRequests: req.body.specialRequests,
       eventLocation: req.body.eventLocation,
-      accommodations:req.body.accommodations,
+      accommodations: req.body.accommodations,
       totalPrice: req.body.totalPrice,
     });
     const updateData = {
