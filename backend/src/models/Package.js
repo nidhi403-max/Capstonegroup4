c12ad0c4ef20daa62ac53aec8649bb082f83d775
nidhi403-max <83553500+nidhi403-max@users.noreply.github.com>
2024-04-06 16:38:01 -0400

    Delete backend directory

--- backend/src/models/Package.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const packageSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-});
-
-module.exports = mongoose.model('Package', packageSchema);
--- backend/src/routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/emailController');
-
-router.post('/sendemail', emailController.sendEmail);
-
-
-module.exports = router;
